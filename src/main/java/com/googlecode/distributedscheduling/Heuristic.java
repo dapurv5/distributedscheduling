8f417d7d1f0e37714b0ca6d7786f541257de4c98
dpmahato <dpmahato@users.noreply.github.com>
2017-11-25 10:53:55 +0530

    Update Heuristic.java

--- src/main/java/com/googlecode/distributedscheduling/Heuristic.java
+++ src/main/java/com/googlecode/distributedscheduling/Heuristic.java
@@ -5,12 +5,10 @@ package com.googlecode.distributedscheduling;
  * @author apurv verma
  */
 public enum Heuristic {
-    MET,
-    MCT,
-    MaxMin,
-    Sufferage,
-    MinMin,
-    MinMean,
-    MinVar,
-    NovelI,
+    EO,
+    GA,
+    HLBA,
+    DLB,
+    Randomized 
+
 }
